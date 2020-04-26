4303b6731894478b318299738eea9faf9ddd1177
Finn Siegismund <Finn Siegismund@Finn.fritz.box>
2020-04-26 13:38:36 +0200

    Hello Europe Test

---
+++ HelloEurope/src/EuropePackage/HelloEurope.java
@@ -0,0 +1,10 @@
+package EuropePackage;
+
+public class HelloEurope {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Europe!");
+	}
+
+}
