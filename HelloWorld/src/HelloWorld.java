a0d44e89f7d37700630e6c6fc99742876f9820bc
Finn Siegismund <Finn Siegismund@Finn.fritz.box>
2020-04-26 13:33:19 +0200

    New wonderful tests!

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to my test project!");
+		System.out.println("With wonderful test Projects :)");
 
 	}
 
